b67d3d73ce149085b83052ca5152bd61e078fba0
Antoni Nicyfor <ant.nicyf@gmail.com>
2020-04-18 15:03:10 -0700

    player and system interaction

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -1,14 +1,24 @@
 using Godot;
 using System;
 
+public enum PlayerState
+{
+    Idle,
+    Minigame,
+    Bridge
+}
+
 public class PlayerController : KinematicBody2D
 {
+    public PlayerState state = PlayerState.Idle;
     public int Speed = 200;
+    bool InReplicatorRoom = false;
 
     Vector2 velocity = new Vector2();
 
     public void GetInput()
     {
+        
         velocity = new Vector2();
 
         if (Input.IsActionPressed("right"))
@@ -28,7 +38,33 @@ public class PlayerController : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        GetInput();
-        velocity = MoveAndSlide(velocity);
+        switch (state)
+        {
+            case PlayerState.Idle:
+                GetInput();
+                velocity = MoveAndSlide(velocity);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OnReplicatorInteract()
+    {
+        if (InReplicatorRoom)
+        {
+            state = PlayerState.Minigame;
+            //TODO: load minigame scene somehow
+        }
+    }
+
+    public void ReplicatorRoomEntered(Node body)
+    {
+        InReplicatorRoom = true;
+    }
+
+    public void ReplicatorRoomExited(Node body)
+    {
+        InReplicatorRoom = false;
     }
 }
